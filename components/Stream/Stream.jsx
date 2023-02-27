f41a49904cc58850e486713c0816d0d343ee55c0
Samir Raza Windows Laptop <samirraza09@gmail.com>
2023-02-27 16:13:12 -0700

    Some work on the stream

--- components/Stream/Stream.jsx
+++ components/Stream/Stream.jsx
@@ -1,16 +1,28 @@
+import getInbox from "./getInbox";
+import React, { useState, useEffect } from 'react';
+
 export default function Stream({ inbox }) {
+  const [stream, setStream] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/authors/4770a8cd-e8b7-434b-b818-e9bea84af2a0/inbox")
+    .then((res) => res.json())
+    .then((json) => {
+        setStream({
+            items: json,
+            DataisLoaded: true
+        });
+    })
+  }, []);
+  console.log(stream);
   return (
-    <div>
-      {inbox.map((post) => (
-        <div key={post.id}>
-          <h1>{post.author}</h1>
-          <p>{post.text}</p>
-          <div>
-            Likes: {post.likes}
-            <button className="p-3">Like</button>
+    <div class="flex flex-col ...">
+      {
+        stream.items?.items?.map((post) => (
+          <div key={post.id} class="m-auto">
+            Hello
           </div>
-        </div>
-      ))}
+        ))
+      }
     </div>
-  );
+  )
 }
