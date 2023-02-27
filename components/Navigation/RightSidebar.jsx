ed8b3fdcbb7d66dffb9db944752108f9bcaf868e
charith-pathi <c_pathirathna@hotmail.com>
2023-02-27 15:10:43 -0700

    Added initial homepage design

---
+++ components/Navigation/RightSidebar.jsx
@@ -0,0 +1,12 @@
+import Card from '@epicapp/components/Card';
+
+export default function LeftSidebar() {
+    return (
+        <Card>
+            <div className="px-4 py-2">
+            <h2 className="text-gray-400 font-bold mb-4">Friend Requests</h2>
+            <a href="" className="flex gap-2 py-3"></a>
+            </div>
+        </Card>
+    )
+}
