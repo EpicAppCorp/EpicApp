fb39abd1bc2074fbe24ed416567543bcd9a5c4a6
Samir Raza Windows Laptop <samirraza09@gmail.com>
2023-03-01 17:40:33 -0700

    Made stuff

--- components/Stream/Stream.jsx
+++ components/Stream/Stream.jsx
@@ -1,8 +1,9 @@
-import getInbox from "./getInbox";
+import Card from '../Card';
 import React, { useState, useEffect } from 'react';
 
 export default function Stream({ inbox }) {
   const [stream, setStream] = useState([]);
+  const [objContent, setObjectContent] = useState()
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/authors/4770a8cd-e8b7-434b-b818-e9bea84af2a0/inbox")
     .then((res) => res.json())
@@ -12,17 +13,35 @@ export default function Stream({ inbox }) {
             DataisLoaded: true
         });
     })
-  }, []);
+  }, [setStream]);
+
+  stream.items?.items.forEach((post) => {
+    fetch(post.object)
+    .then((res) => res.json())
+    .then((json) => {
+        post.objectContent = json;
+    })
+  });
   console.log(stream);
+
   return (
-    <div class="flex flex-col ...">
+    <Card>
+      <div className="divide-y ...">
       {
         stream.items?.items?.map((post) => (
-          <div key={post.id} class="m-auto">
-            Hello
+          <div key={post.id} className="m-auto p-5 ">
+            <div>{post.summary}</div>
+            { 
+              <div>
+                {
+                  post.objectContent?.description
+                }
+              </div>
+            }
           </div>
         ))
       }
-    </div>
+      </div>
+    </Card>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,6 @@ import RightSidebar from '@epicapp/components/Navigation/RightSidebar.jsx';
 import CreatePost from '@epicapp/components/CreatePost/CreatePost.jsx';
 
 export default function Home() {
-  // const inbox = [
-  //   {
-  //     id: 123,
-  //     author: 'Samir Raza',
-  //     privacy: 'public',
-  //     type: 'text',
-  //     text: 'This is text for a post',
-  //     likes: 10,
-  //     comments: [
-  //       {
-  //         commenterId: 12,
-  //         commentText: 'This is a comment',
-  //       },
-  //       {
-  //         commenterId: 21,
-  //         commentText: 'This is another comment',
-  //       },
-  //     ],
-  //   },
-  // ];
   return (
     <div className="flex mt-4 max-w-6xl mx-auto gap-6">
       <div className="w-1/5">
@@ -35,7 +15,7 @@ export default function Home() {
       </div>
       <div className="grow">
         <CreatePost />
-        <Card>inbox stream...</Card>
+        <Stream />
       </div>
       <div className="w-1/5">
         <RightSidebar />  
