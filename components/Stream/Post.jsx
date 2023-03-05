b3cda89146dab04f8682c35343aacc36d841d6b1
Samir Raza Windows Laptop <samirraza09@gmail.com>
2023-03-04 18:49:46 -0700

    Can now like posts

--- components/Stream/Post.jsx
+++ components/Stream/Post.jsx
@@ -5,14 +5,19 @@ import axios from 'axios';
 export default function Post({post}) {
     const [comments, setComments] = useState();
     const [comment, setComment] = useState("");
+    const [likes, setLikes] = useState([]);
     useEffect(() => {
         axios.get(post.id + "/comments")
         .then((res) =>  {
             setComments(res.data);
         })
+        axios.get(post.id + "/likes")
+        .then((res) => {
+            setLikes(res.data)
+        })
     })
     const CommentSubmit = () => {
-        console.log(post);
+        console.log(likes);
         axios.post(post.id + "/comments",{
             "comment": comment,
             "post_id": post.id,
@@ -26,6 +31,24 @@ export default function Post({post}) {
                 "profile_image": "https://i.imgur.com/k7XVwpB.jpeg"
             },
             "author_id": "http://localhost:8000/api/authors/c2900cf7-a019-48f7-822a-ffa4023aba91"
+        });
+    }
+    const LikePost = () => {
+        const inboxEndpoint = post.id.split('/posts')[0] + "/inbox";
+        axios.post(inboxEndpoint ,{
+            "@context": "https://www.w3.org/ns/activitystreams",
+            "summary": "Nathan Drake liked your post Likes your post",
+            "type": "Like",
+            "author": {
+                "type": "author",
+                "id": "c2900cf7-a019-48f7-822a-ffa4023aba91", // Temporarily hardcoded
+                "host": "http://127.0.0.1:5454/",
+                "displayName": "Jim Jimbo",
+                "url": "http://127.0.0.1:5454/authors/1d698d25ff008f7538453c120f581471",
+                "github": "http://github.com/jjimbo",
+                "profile_image": "https://i.imgur.com/k7XVwpB.jpeg"
+            },
+            "object": post.id
         });
     }
     return (
@@ -50,16 +73,21 @@ export default function Post({post}) {
                     onClick={CommentSubmit}>
                     <p className="font-black">→</p>
                 </button>
-                <button className="bg-submitBg text-white px-4 py-1 rounded-md ">
+                <button 
+                    className="bg-submitBg text-white px-4 py-1 rounded-md"
+                    onClick={LikePost}>
                     <p className="font-black">♥</p>
                 </button>
+                <div className="ml-4 m-auto" >
+                    Likes: {likes.length}
+                </div>
             </div>
             <div className="m-4">
                 <div>Comments:</div>
                 {
                     comments?.comments.map((comment) => {
                         return (
-                            <div>{comment.author.displayName}: {comment.comment}</div>
+                            <div key={comment.id}>{comment.author.displayName}: {comment.comment}</div>
                         )
                     })
                 }
