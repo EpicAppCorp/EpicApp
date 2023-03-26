326430942f747413880823d69c763521778958e1
Shane Goonasekera <theshanukg@gmail.com>
2023-03-25 23:13:46 -0600

    fixed comment stuff

--- services/comment.js
+++ services/comment.js
@@ -4,8 +4,8 @@ export const getComments = (url) =>
   axiosClient.get(url + '/comments?page=1&size=1000');
 
 export const newComment = (post) => {
-  if (post.author.id.includes(process.env.NEXT_PUBLIC_API)) {
-    return axiosClient.post(post.post_id + '/comments/', post);
+  if (post.post.includes(process.env.NEXT_PUBLIC_API)) {
+    return axiosClient.post(post.post + '/comments/', post);
   }
-  return axiosClient.post(post.author.id + '/inbox', post);
+  return axiosClient.post(post.author + '/inbox', post);
 };
