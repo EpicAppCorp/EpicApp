08a483c2f74953a7c8af906839ed0a0af2cf7dfc
Shane Goonasekera <theshanukg@gmail.com>
2023-04-02 23:15:57 -0600

    small fixes to each group

--- services/comment.js
+++ services/comment.js
@@ -4,7 +4,7 @@ export const getComments = (url) =>
   getAxiosInstance(url).get(url + '/comments?page=1&size=20');
 
 export const newComment = (author, post) =>
-  getAxiosInstance(post.author.id).post(post.author.id + '/inbox/', {
+  getAxiosInstance(post.author.url).post(post.author.url + '/inbox/', {
     type: 'comment',
     contentType: 'text/plain',
     comment: post.comment,
--- services/like.js
+++ services/like.js
@@ -3,7 +3,7 @@ import { getAxiosInstance } from '@epicapp/libs/axios';
 export const getLikes = (url) => getAxiosInstance(url).get(url + '/likes');
 
 export const newLike = (author, item) =>
-  getAxiosInstance(item.author.id).post(item.author.id + '/inbox/', {
+  getAxiosInstance(item.author.url).post(item.author.url + '/inbox', {
     type: 'Like',
     author: author.id,
     object: item.object,
